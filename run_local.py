831c98f2d80a314c91e6da694d8ae223963d6f89
beharmedrano <52407293+beharmedrano@users.noreply.github.com>
2020-01-15 21:50:38 +0200

    Create run_local.py

---
+++ run_local.py
@@ -0,0 +1,8 @@
+import quant_verify
+import analysis
+import argparse
+
+
+def experiment_atl1_ex1:
+    quant_verify.experiment_run_at_least_to_at_least_or_most(testDir=testDir, atl=atl, atl2=atl2, atm=atm);
+                    analysis.analysis_on_dir(dirName=dirName, filename=filename, atl=atl, atl2=atl2, atm=atm)
